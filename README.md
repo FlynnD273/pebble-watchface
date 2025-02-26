6416f8c8fdfda0bae7b35b988e2aad9acf707a4f
Flynn Duniho <flynn@harveyosity.org>
2025-02-26 18:09:54 -0500

    Basic watchface has been made

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# classio
+# Pebble Watchface
 
-![screenshot](classio-battery-connection-screenshot.png)
-
-Example watchface showing the time, including seconds.
-
-This version also displays the battery level and status of the Bluetooth
-connection.
+A custom watchface made from the example.
---
+++ src/c/main.c
@@ -0,0 +1,107 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static TextLayer *s_minute_layer;
+static TextLayer *s_hour_layer;
+static Layer *s_battery_layer;
+static GFont s_font;
+static uint8_t batt_percent = 0;
+
+static void handle_battery(BatteryChargeState charge_state) {
+  batt_percent = charge_state.charge_percent;
+  layer_mark_dirty(s_battery_layer);
+}
+
+static void battery_update_proc(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx,
+                     GRect(0, bounds.origin.y,
+                           bounds.size.w * batt_percent / 100, bounds.size.h),
+                     0, 0);
+}
+
+static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+  // Needs to be static because it's used by the system later.
+  static char s_time_text[] = "00";
+  tick_time->tm_hour %= 12;
+
+  strftime(s_time_text, sizeof(s_time_text), "%02M", tick_time);
+  text_layer_set_text(s_minute_layer, s_time_text);
+}
+static void handle_hour_tick(struct tm *tick_time, TimeUnits units_changed) {
+  // Needs to be static because it's used by the system later.
+  static char s_time_text[] = "00";
+
+  strftime(s_time_text, sizeof(s_time_text), "%02l", tick_time);
+  text_layer_set_text(s_hour_layer, s_time_text);
+}
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  s_minute_layer = text_layer_create(GRect(bounds.origin.x,
+                                           bounds.origin.y + bounds.size.h / 2,
+                                           bounds.size.w, bounds.size.h / 2));
+  text_layer_set_text_color(s_minute_layer, GColorWhite);
+  text_layer_set_background_color(s_minute_layer, GColorClear);
+  text_layer_set_font(s_minute_layer, s_font);
+  text_layer_set_text_alignment(s_minute_layer, GTextAlignmentCenter);
+
+  s_hour_layer = text_layer_create(GRect(bounds.origin.x, bounds.origin.y,
+                                         bounds.size.w, bounds.size.h / 2));
+  text_layer_set_text_color(s_hour_layer, GColorWhite);
+  text_layer_set_background_color(s_hour_layer, GColorClear);
+  text_layer_set_font(s_hour_layer, s_font);
+  text_layer_set_text_alignment(s_hour_layer, GTextAlignmentCenter);
+
+  s_battery_layer = layer_create(GRect(0, bounds.size.h / 2, bounds.size.w, 5));
+  layer_set_update_proc(s_battery_layer, battery_update_proc);
+
+  // Ensures time is displayed immediately (will break if NULL tick event
+  // accessed). (This is why it's a good idea to have a separate routine to do
+  // the update itself.)
+  time_t now = time(NULL);
+  struct tm *current_time = localtime(&now);
+  handle_minute_tick(current_time, SECOND_UNIT);
+
+  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+  tick_timer_service_subscribe(HOUR_UNIT, handle_hour_tick);
+  battery_state_service_subscribe(handle_battery);
+
+  layer_add_child(window_layer, text_layer_get_layer(s_minute_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_hour_layer));
+  layer_add_child(window_layer, s_battery_layer);
+
+  handle_battery(battery_state_service_peek());
+}
+
+static void main_window_unload(Window *window) {
+  tick_timer_service_unsubscribe();
+  battery_state_service_unsubscribe();
+  connection_service_unsubscribe();
+  text_layer_destroy(s_minute_layer);
+  text_layer_destroy(s_hour_layer);
+  layer_destroy(s_battery_layer);
+}
+
+static void init() {
+  s_font =
+      fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CHIVO_61));
+  s_main_window = window_create();
+  window_set_background_color(s_main_window, GColorBlack);
+  window_set_window_handlers(s_main_window, (WindowHandlers){
+                                                .load = main_window_load,
+                                                .unload = main_window_unload,
+                                            });
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit() { window_destroy(s_main_window); }
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
